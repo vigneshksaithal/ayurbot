cd7bf07364c11ce3b2bc90b22e40fcb9c4887dbc
VIGNESH K S <iamvvv0@gmail.com>
2021-11-09 21:21:32 +0530

    Remove svelte comments

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import logo from './assets/svelte.png';
+  import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
@@ -8,16 +8,6 @@
   <h1>Hello world!</h1>
 
   <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
 </main>
 
 <style>
